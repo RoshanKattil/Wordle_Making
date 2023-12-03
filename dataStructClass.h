4c7a7fbf6dafb3c872bb77752f6ac2b1ef12eab0
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-12-02 20:22:18 -0800

    Update dataStructClass.h

--- dataStructClass.h
+++ dataStructClass.h
@@ -1,1 +1,43 @@
+#ifndef dataStructClass
+#define dataStructClass.h
 
+#include <iostream>
+
+using namespace std;
+
+
+    class wordleScreen
+    {
+      public:
+        string inputString;
+        string guessedWord;
+        int attempts;
+        
+
+        void do_something();
+
+        // function that gets input
+
+        void input_verification()
+        {
+            // if input is a verifable word guessedWord = input
+              // function that verifies input
+              //attempts++;
+        }
+
+      // function that sorts letters into green, yellow, and grey
+
+      
+
+
+      private:
+        string correctWord;
+
+        //function to get random word
+
+        
+        
+    };
+
+
+#endif
