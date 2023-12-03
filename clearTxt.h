067d76aec8c0cebd1c0a4f03dc3f83026159ba68
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-12-02 19:12:28 -0800

    Create clearTxt.h

---
+++ clearTxt.h
@@ -0,0 +1,32 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+bool clearFile(string fileName)
+{
+
+  bool isCleared = false;
+
+  //takes in file name and clears the file
+  ofstream ofs;
+  ofs.open(fileName, ofstream::out | ofstream::trunc);
+  ofs.close();
+
+  //check if succsesful if yes then return true
+  ifstream is;
+  is.open (fileName.c_str(), ios::binary);
+  is.seekg (0, ios::end);
+  length = is.tellg();
+
+  // check if number of charachters in file is 0 then 
+  if (length == 0)
+  {
+    isCleared = true;
+  }
+
+  return isCleared;
+  
+}
+
+
+
+#endif 
