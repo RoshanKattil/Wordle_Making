f5add1b1186b06c1070a8fdd529be4446663339b
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-12-03 01:04:48 -0800

    Update worldle.cpp

--- worldle.cpp
+++ worldle.cpp
@@ -16,6 +16,28 @@ int main ()
 
     // initalize the headler file with the class for wordle
 
+    string break_case;
+    bool valid_input;
+
+    wordleGame game;
+    game.word_generation();
+
+
+    while(!(game.gave_over()) && !(break_case == "x"))
+    {
+        do
+        {
+            break_case = game.get_input();
+        
+            if(break_case == "x")
+            {break;}
+    
+            valid_input = game.input_verification()
+        }
+        while(!valid_input);
+    }
+    
+
     // initalize keyboard
        // ask the user to input words onto the keyboard file
        // then check is valid then put that  into a shared file which is accsesed
