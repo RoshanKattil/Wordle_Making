f0d6fb79ad7c0a511be43b89c2bd51694a8c92d1
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-12-02 20:51:02 -0800

    Create word_checker.h
    setting up search algorithm

---
+++ word_checker.h
@@ -0,0 +1,44 @@
+#ifndef word_checker
+#define word_checker
+
+#include <string>;
+#include <iostream>;
+
+using namespace std:
+
+//a's are at 141
+//b's are at 314
+//c's are at 512
+//d 623
+//e 695
+//f 831
+//g 946
+//h 1015
+//i 1049
+//j 1069
+//k 1089
+//l 1177
+//m 1284
+//n 1321
+//o 1362
+//p 1504
+//q 1527
+//r 1632
+//s 1998
+//t 2147
+//u 2180
+//v 2223
+//w 2306
+//y 2312
+//z 2315
+
+bool wordChecker(string word)
+{
+
+  
+
+}
+
+
+
+#endif
