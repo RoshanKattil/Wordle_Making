0239021ada230407bb28b02cc7923e4f73d4245d
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-12-02 18:37:51 -0800

    Create worldle.cpp

---
+++ worldle.cpp
@@ -0,0 +1,31 @@
+// have wordle.cpp run by menu.cpp file
+
+// incide header file
+// includes class
+
+
+// inculde imports
+
+
+#include<iostream>
+
+using namespace std;
+
+int main () 
+{
+
+    // initalize the headler file with the class for wordle
+
+    // initalize keyboard
+       // ask the user to input words onto the keyboard file
+       // then check is valid then put that  into a shared file which is accsesed
+
+  
+ 
+  
+
+  
+  
+  
+    return 0;
+}
