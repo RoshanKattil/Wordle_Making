4bb525b7322ea7ce7e7e3d26461143b79268cf59
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-12-02 22:03:38 -0800

    Update word_checker.h
    make index for allowed.txt instead of words.txt later

--- word_checker.h
+++ word_checker.h
@@ -1,6 +1,7 @@
 #ifndef word_checker
 #define word_checker
 
+#include <fstream>
 #include <string>;
 #include <iostream>;
 
@@ -32,23 +33,39 @@ using namespace std:
 //y 2312
 //z 2315
 
-bool wordChecker(string word)
+bool wordChecker(string word, &vector<string> allowed)
 {
 
+  //set to word change to allowed
   const int index[25] = {141, 314, 512, 623, 695, 831, 946, 1015, 1049, 1069, 1089, 1177, 1284, 1321, 1362, 1504, 1527, 1632, 1998, 2147, 2180, 2223, 2306, 2312, 2315};
 
   //converting the first char to ascii then subtract by 97 to get it mapped from 1-27
-  int asciiValue = int(word[0])-97;
+  int asciiValue_0 = int(word[0])-97;
+  int asciiValue_1 = int(word[1])-97;
 
   if(word[0] == x) // checks if first charachter is not x
   {
     return false;
   )
-  else if((asciiValue <= 25) && (asciiValue >= 0)) //checks if first charachter is a letter
+  else if((asciiValue_0 <= 25) && (asciiValue_0 >= 0)) //checks if first charachter is a letter
   {
+      // decides to start looking at start or end of subset of vector
+      if(asciiValue_1 < 9)
+      {
 
+      for(int i = (asciiValue_0 - 1); i < asciiValue_1; i++)
+      {if(word == allowed[i])
+        {return true;}
+      }
+      else
+      {
+        for(int i = (asciiValue_0 - 1); i < asciiValue_1; i++)
+          {if(word == allowed[i])
+            {return true;}}
+      }
+
+    }
     
-
   }
   else
   {
